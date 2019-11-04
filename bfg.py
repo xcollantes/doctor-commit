15b87099e0535a768add96d2915fc9e5c8ab62ec
Ubuntu <collantes.xavier@gmail.com>
2019-11-04 05:29:15 +0000

    Use bash script

---
+++ bfg.py
@@ -0,0 +1,22 @@
+"""Wrapper for BFG"""
+
+import os
+import subprocess
+import argparse
+
+
+def main():
+  parser = argparse.ArgumentParser(
+            description='Wrapper script for cleaning up Git commits.',
+            )
+  #parser.add_argument('delete-files' )
+  #parser.add_argument('remove-text',)
+   
+  jar = os.path.abspath('bfg-1.13.0.jar')
+  print(jar)
+
+
+
+
+if __name__=='__main__':
+  main()
